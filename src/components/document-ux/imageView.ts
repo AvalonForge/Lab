1e7f267219bebc86cad3afda541bf19679db8f29
Timmy <>
2022-10-03 19:18:42 -0400

    some little fixes to image view

--- src/components/document-ux/imageView.ts
+++ src/components/document-ux/imageView.ts
@@ -61,7 +61,7 @@ export const imageView = new Plugin({
             }
           });
         } else {
-          dom.className = "rounded-3 px-4 py-3 border";
+          dom.className = "rounded-2 px-3 py-2 border";
           dom.style.borderColor = "var(--type-color)";
           dom.addEventListener("drop", (event) => {
             if (!event.dataTransfer) return;
@@ -88,7 +88,7 @@ export const imageView = new Plugin({
               };
             }
           });
-          const guard = document.createElement("doc");
+          const guard = document.createElement("div");
           guard.contentEditable = "false";
           guard.style.display = "flex";
           const input = document.createElement("input");
@@ -114,6 +114,30 @@ export const imageView = new Plugin({
             }
           });
           guard.appendChild(input);
+
+          const submit = document.createElement("div");
+          submit.innerHTML = `
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              fill="var(--type-color)"
+              class="icon clickable"
+            >
+              <path d="M211.8 339.8C200.9 350.7 183.1 350.7 172.2 339.8L108.2 275.8C97.27 264.9 97.27 247.1 108.2 236.2C119.1 225.3 136.9 225.3 147.8 236.2L192 280.4L300.2 172.2C311.1 161.3 328.9 161.3 339.8 172.2C350.7 183.1 350.7 200.9 339.8 211.8L211.8 339.8zM0 96C0 60.65 28.65 32 64 32H384C419.3 32 448 60.65 448 96V416C448 451.3 419.3 480 384 480H64C28.65 480 0 451.3 0 416V96zM48 96V416C48 424.8 55.16 432 64 432H384C392.8 432 400 424.8 400 416V96C400 87.16 392.8 80 384 80H64C55.16 80 48 87.16 48 96z"/>
+            </svg>
+          `;
+          submit.addEventListener("click", (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            const tr = view.state.tr.setNodeMarkup(
+              getPos(),
+              schema.nodes["image"],
+              { src: input.value, height: "auto" }
+            );
+            view.dispatch(tr);
+          });
+          guard.appendChild(submit);
+
           dom.appendChild(guard);
         }
 
